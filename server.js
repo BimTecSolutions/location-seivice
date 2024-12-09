dc60d4207964e83633d926c955fdab1cafd8eb4f
BimTecSolutions <92627623+BimTecSolutions@users.noreply.github.com>
2024-12-09 09:15:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -205,14 +205,14 @@ app.get('/request-location', async (req, res) => {
 // Route for sending subscription action
 app.post('/send-subscription-action', async (req, res) => {
   const subscriptionActionPayload = {
-    "applicationId": "APP_008542",
-    "password": "d927d68199499f5e7114070bf88f9e6e",
-    "subscriberId": "te:94713181860",
+    "applicationId": "APP_066319",
+    "password": "c182dd009972ed36c0734af861b596dc",
+    "subscriberId": "te:94767544774",
     "action": "1"
   };
 
   try {
-    const subscriptionActionResponse = await axios.post('https://api.mspace.lk/subscription/send', subscriptionActionPayload, {
+    const subscriptionActionResponse = await axios.post('https://api.dialog.lk/subscription/send', subscriptionActionPayload, {
       headers: { 'Content-Type': 'application/json' },
       proxy: {
         host: parsedUrl.hostname,
@@ -363,9 +363,9 @@ app.get('/', (req, res) => {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-              applicationId: "APP_008542",
-              password: "d927d68199499f5e7114070bf88f9e6e",
-              subscriberId: "te:94713181860",      
+              applicationId: "APP_066319",
+              password: "c182dd009972ed36c0734af861b596dc",
+              subscriberId: "te:94767544774",      
               action: "1"
             })
           });
